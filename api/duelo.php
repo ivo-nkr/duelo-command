e7ad6fba63d16b2b335b3a687bd795c09c1e49a8
ivo-nkr <166124254+ivo-nkr@users.noreply.github.com>
2024-08-21 11:26:34 -0300

    add duelo

---
+++ api/duelo.php
@@ -0,0 +1,13 @@
+<?php
+    $respuestas = array(
+        "esta",
+        "es",
+        "una",
+        "prueba",
+        "de",
+        "comando"
+        );
+    $random = array_rand($respuestas);
+    echo $respuestas[$random];
+    ?>
+                            
